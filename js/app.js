ed02bea65d895b06ae220c78fedba81adcabb75e
JABALE NOOR SHAHBAJ <69644198+shahbaj00@users.noreply.github.com>
2022-03-04 16:34:21 +0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,15 +1,44 @@
 function updateConfig(item, amount) {
     const itemAmount = document.getElementById(item + '-cost');
     //update Extra Costs
-    if (amount == 0) {
-        itemAmount.innerText = 0;
-    } else if (amount == 20) {
-        itemAmount.innerText = 20;
-    } else if (amount == 100) {
-        itemAmount.innerText = 100;
-    } else if (amount == 180) {
-        itemAmount.innerText = 180;
+    switch (amount) {
+        case 0:
+            itemAmount.innerText = 0;
+            break;
+        case 20:
+            itemAmount.innerText = 20;
+            break;
+        case 100:
+            itemAmount.innerText = 100;
+            break;
+        case 180:
+            itemAmount.innerText = 180;
+            break;
     }
+}
+const activeStyle = (item, type) => {
+    //active button style
+    const classList = document.querySelectorAll(`.${item}-btn`);
+    for (const className of classList) {
+        className.style.backgroundColor = "white";
+        className.style.color = 'black'
+    }
+
+
+    if (item == 'memory') {
+        const itemType = document.getElementById(`${type}-memory-btn`);
+        itemType.style.backgroundColor = "#1f2937"
+        itemType.style.color = 'white';
+    } else if (item == 'storage') {
+        const itemType = document.getElementById(`${type}-ssd-btn`);
+        itemType.style.backgroundColor = "#1f2937"
+        itemType.style.color = 'white';
+    } else if (item == 'delivery') {
+        const itemType = document.getElementById(`${type}-delivery-btn`);
+        itemType.style.backgroundColor = "#1f2937"
+        itemType.style.color = 'white';
+    }
+
 }
 
 // Get Extra configuration amount
@@ -37,30 +66,37 @@ function updateBalance() {
 document.getElementById('8gb-memory-btn').addEventListener('click', function() {
     updateConfig('memory', 0);
     updateBalance()
+    activeStyle('memory', '8gb');
 });
 document.getElementById('16gb-memory-btn').addEventListener('click', function() {
     updateConfig('memory', 180);
     updateBalance()
+    activeStyle('memory', '16gb');
 });
 document.getElementById('256gb-ssd-btn').addEventListener('click', function() {
     updateConfig('storage', 0);
     updateBalance()
+    activeStyle('storage', '256gb');
 });
 document.getElementById('512gb-ssd-btn').addEventListener('click', function() {
     updateConfig('storage', 100);
     updateBalance()
+    activeStyle('storage', '512gb');
 });
 document.getElementById('1tb-ssd-btn').addEventListener('click', function() {
     updateConfig('storage', 180);
     updateBalance()
+    activeStyle('storage', '1tb');
 });
 document.getElementById('free-delivery-btn').addEventListener('click', function() {
     updateConfig('delivery', 0);
     updateBalance()
+    activeStyle('delivery', 'free');
 });
-document.getElementById('paid-delivery-btn').addEventListener('click', function() {
+document.getElementById('pay-delivery-btn').addEventListener('click', function() {
     updateConfig('delivery', 20);
     updateBalance()
+    activeStyle('delivery', 'pay');
 });
 document.getElementById('pomo-code-btn').addEventListener('click', function() {
     //get pomo Input and total price
